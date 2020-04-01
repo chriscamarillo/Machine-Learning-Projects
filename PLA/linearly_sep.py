2640bd20688874a9f0f7dc137265b67b0c872edc
aig77 <agalan2021@gmail.com>
2020-04-01 19:05:13 -0400

    generate dataset file

--- PLA/linearly_sep.py
+++ PLA/linearly_sep.py
@@ -1,20 +1,22 @@
 from random import random
-
-def generateDataset(start, end, diff, lo, hi, linearly_sep = True):
+# random range = r_max - r_min
+# diff = amount values can differ from randomly generated line
+# first_half + second_half = size of dataset
+def generateDataset(r_min, r_max, diff, first_half, second_half, linearly_sep = True):
     if linearly_sep == True:
         # coeff[0] represents y = coeff[0]x + coeff[1]
-        coeff = [random() * ((end - start + 1) - start) for x in range(2)]
+        coeff = [random() * ((r_max - r_min + 1) - r_min) for x in range(2)]
         # [x, y, class]
         dataset = []
         i = 0
-        while i < lo:
+        while i < first_half:
             x = random() * diff
             y = x * coeff[0] + coeff[1] - random() * diff
             dataset.append([x, y, 0])
             i += 1
         
         j = 0
-        while j < hi:
+        while j < second_half:
             x = random() * diff
             y = x * coeff[0] + coeff[1] + random() * diff
             dataset.append([x, y, 1])
@@ -22,11 +24,17 @@ def generateDataset(start, end, diff, lo, hi, linearly_sep = True):
 
         return dataset
 
+def generateDatasetFile(dataset, filename):
+    f = open(f"datasets/{filename}", 'w')
+    for v in dataset:
+        f.write(','.join(str(i) for i in v) + '\n')
+
 
 def main():
-    pipi = generateDataset(1, 10, 25, 50, 50, True)
-    for v in pipi:
+    d = generateDataset(1, 10, 25, 50, 50, True)
+    for v in d:
         print(v)
+    generateDatasetFile(d, "toyset.data")
 
 if __name__ == "__main__":
     main()
